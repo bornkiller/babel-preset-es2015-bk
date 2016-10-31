21f0d970cb23a3a4ab5afb9ae9e9c7bdcbb11be1
黄健 <jian.huang03@ele.me>
2016-10-31 15:37:37 +0800

    feature: 调整插件列表。

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+/**
+ * @description - personal es2015-preset
+ * @author - bornkiller <hjj491229492@hotmail.com>
+ */
 var modify = require('modify-babel-preset');
 
 module.exports = modify('es2015', {
-	// remove commonjs transform
-	'transform-es2015-modules-commonjs': false,
+  // remove commonjs transform
+  'transform-es2015-modules-commonjs': false,
 
-	// add external helpers
-	'external-helpers': true
+  // remove generator not used
+  'transform-regenerator': false,
+
+  // support static property
+  'transform-class-properties': true,
+
+  // support external helpers
+  'external-helpers': !process.env.SKIP_EXTERNAL
 });
