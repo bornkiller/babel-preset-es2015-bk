ce6a52acda4242dd62c162f164da9575862eea9e
黄健 <jian.huang03@ele.me>
2016-11-01 11:32:32 +0800

    fix: 修复transform-class-properties失效问题。

--- index.js
+++ index.js
@@ -2,18 +2,24 @@
  * @description - personal es2015-preset
  * @author - bornkiller <hjj491229492@hotmail.com>
  */
-var modify = require('modify-babel-preset');
 
-module.exports = modify('es2015', {
-  // remove commonjs transform
+/**
+ * @description
+ * - use within package bundle, never expect generate too much external code
+ * - means i don't need external helpers
+ * - transform-class-properties need run before transform-classed
+ */
+var modify = require('modify-babel-preset');
+var transformClassProperties = require('babel-plugin-transform-class-properties');
+
+var preset = modify('es2015', {
+  // remove commonjs transform for tree-shaking compatible
   'transform-es2015-modules-commonjs': false,
 
   // remove generator not used
-  'transform-regenerator': false,
-
-  // skip external helpers node environment
-  'external-helpers': false,
-
-  // support static property
-  'transform-class-properties': true
+  'transform-regenerator': false
 });
+
+preset.plugins.unshift(transformClassProperties);
+
+module.exports = preset;
